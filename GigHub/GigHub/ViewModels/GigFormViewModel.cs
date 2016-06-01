04c91a6ceb0dacc9be64137737167a75eb0ef0fb
Christopher Catilo <chriscatilo@hotmail.com>
2016-06-01 16:48:45 +0100

    Extracting a View Model

---
+++ GigHub/GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+
+        public string Date { get; set; }
+
+        public string Time { get; set; }
+    }
+}
