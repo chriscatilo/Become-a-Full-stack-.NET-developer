26b7942175ef1e8c0b056dc11df923c06d4371e6
Christopher Catilo <chriscatilo@hotmail.com>
2016-06-02 15:22:55 +0100

    Adding Following button and wiring up to API

---
+++ GigHub/GigHub/Controllers/FollowingsController.cs
@@ -0,0 +1,43 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    public class FollowingsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public FollowingsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            var exists = _context.Followings.Any(f => f.FolloweeId == userId && f.FolloweeId == dto.FolloweeId);
+            if (exists)
+                return BadRequest("Following already exists");
+
+            var following = new Following()
+            {
+                FollowerId = userId,
+                FolloweeId = dto.FolloweeId
+            };
+
+            _context.Followings.Add(following);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+
+    public class FollowingDto
+    {
+        public string FolloweeId { get; set; }
+    }
+}
