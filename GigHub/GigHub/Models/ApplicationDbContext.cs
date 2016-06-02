a4ce07cb0b4d8d8bef6901417f9d06c4980b201f
Christopher Catilo <chriscatilo@hotmail.com>
2016-06-02 18:24:26 +0100

    Implementing Domain Classes and Migrating the Database

--- GigHub/GigHub/Models/ApplicationDbContext.cs
+++ GigHub/GigHub/Models/ApplicationDbContext.cs
@@ -13,6 +13,10 @@ namespace GigHub.Models
 
         public DbSet<Following> Followings { get; set; }
 
+        public DbSet<Notification> Notifications { get; set; }
+
+        public DbSet<UserNotifcation> UserNotifications { get; set; }
+
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
@@ -39,6 +43,11 @@ namespace GigHub.Models
             modelBuilder.Entity<ApplicationUser>()
                 .HasMany(u => u.Followees)
                 .WithRequired(f => f.Follower)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<UserNotifcation>()
+                .HasRequired(n => n.User)
+                .WithMany()
                 .WillCascadeOnDelete(false);
 
             base.OnModelCreating(modelBuilder);
